541285d9a2b5ecbad8e88072b0baedae8e950d76
nick <nicholas_scalione@yahoo.com>
2021-01-14 21:00:01 -0500

    commit tree 56cb97eef61118054185cb21bb922710e0c4f1af

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+require 'pry'
 
+
+def oxford_comma(kiwi)
+    [kiwi].join
 end
+
+def oxford_comma(array)
+    last_element = array.pop
+    if array.length == 0
+        last_element
+    elsif array.length ==1
+        array.join << " and " + last_element
+    else 
+        array.join(", ") << ", and " + last_element
+    end
+    end
